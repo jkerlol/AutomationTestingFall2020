3ab3edf11a27ab5eb87294de13d4c49e42214d36
Mohammed Arafat <amoh87@ymail.com>
2020-09-14 13:33:26 -0400

    first code examples

---
+++ MyFirstProgram/src/HW1.java
@@ -0,0 +1,30 @@
+public class HW1 {
+	
+	
+	/* HW ASSIGNMENT:
+	 * 
+	 * P1: 
+	 * - Use the calculator class to create a calculator object
+	 * - Use the addition, subtraction, multiplication, and division methods on your new object using 10 and 20 as operators
+	 * - Print all answers to your console
+	 * 
+	 * P2:
+	 * - Use the introduction method (defined below your main method) to print an introduction with your name!
+	 * 
+	 */
+	
+	public static void main (String args[]) {
+		
+
+	}
+	
+	public String introduction (String name) {
+		
+		String result = "";
+		
+		result = "Hi my name is " + name + ", it's a pleasure to meet you!";
+		
+		return result;
+	}
+	
+}
